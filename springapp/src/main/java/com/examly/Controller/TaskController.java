eb3a3bce02889b604fbae6a8364b5f86cf1c8688
Iamneo <techadmin@iamneo.ai>
2023-08-05 16:25:05 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/Controller/TaskController.java
+++ springapp/src/main/java/com/examly/Controller/TaskController.java
@@ -2,20 +2,18 @@ package com.examly.Controller;
 
 import java.util.List;
 
+import com.examly.Exception.ResourceNotFoundException;
+import com.examly.Model.Task;
+import com.examly.Repository.TaskRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.examly.Exception.ResourceNotFoundException;
-import com.examly.Model.Task;
-import com.examly.Repository.TaskRepository;
 
 @RestController
 @RequestMapping("")
--- springapp/src/main/java/com/examly/Model/Task.java
+++ springapp/src/main/java/com/examly/Model/Task.java
@@ -1,4 +1,4 @@
-package com.examly.springapp.model;
+package com.examly.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
